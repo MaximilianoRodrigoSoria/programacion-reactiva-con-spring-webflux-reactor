c9f3bb021885b9cd6d39e9963ae0ab941c12284e
Maximiliano R Soria <maximilianorodrigosoria@gmail.com>
2022-04-21 08:22:41 -0300

    Se agrega log

--- spring-boot-reactor/src/main/java/com/lab/reactor/SpringBootReactorApplication.java
+++ spring-boot-reactor/src/main/java/com/lab/reactor/SpringBootReactorApplication.java
@@ -1,11 +1,13 @@
 package com.lab.reactor;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import reactor.core.publisher.Flux;
 
 @SpringBootApplication
+@Slf4j
 public class SpringBootReactorApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
@@ -16,8 +18,15 @@ public class SpringBootReactorApplication implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
 
-        Flux<String> nombres = Flux.just("Maximiliano", "Bishop", "Eluney")
-                .doOnNext(System.out::println);
-        nombres.subscribe();
+        Flux<String> nombres = Flux.just("Maximiliano", "", "Bishop", "Eluney", "Shinna")
+                .doOnNext(elememto ->
+                        {
+                            if(elememto.isEmpty()){
+                                throw new RuntimeException("No se puede cargar el elemento ya esta vacio");
+                            }
+                            System.out.println(elememto);
+                        }
+                        );
+        nombres.subscribe(e -> log.info(e), error -> log.error(error.getMessage()));
     }
 }
