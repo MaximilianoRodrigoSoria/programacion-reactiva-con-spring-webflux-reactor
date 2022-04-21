f6cb8ab33309dd7c7be8430fe253269e0b59e75e
Maximiliano R Soria <maximilianorodrigosoria@gmail.com>
2022-04-21 08:44:50 -0300

    Filter

--- spring-boot-reactor/src/main/java/com/lab/reactor/SpringBootReactorApplication.java
+++ spring-boot-reactor/src/main/java/com/lab/reactor/SpringBootReactorApplication.java
@@ -1,10 +1,13 @@
 package com.lab.reactor;
 
+import com.lab.reactor.model.Usuario;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import reactor.core.publisher.Flux;
+
+import java.util.Objects;
 
 @SpringBootApplication
 @Slf4j
@@ -18,15 +21,22 @@ public class SpringBootReactorApplication implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
 
-        Flux<String> nombres = Flux.just("Maximiliano", "", "Bishop", "Eluney", "Shinna")
-                .doOnNext(elememto ->
+        Flux<Usuario> nombres = Flux.just("Maximiliano Soria", "Lionel Contrera", "Bishop Soria", "Eluney Contrera", "Shinna Contrera")
+                .map(nombre -> new Usuario(nombre.split(" ")[0], nombre.split(" ")[1]))
+                .doOnNext(usuario ->
                         {
-                            if(elememto.isEmpty()){
-                                throw new RuntimeException("No se puede cargar el elemento ya esta vacio");
+                            if(Objects.isNull(usuario)){
+                                throw new RuntimeException("No se puede cargar el usuario");
                             }
-                            System.out.println(elememto);
+                            System.out.println(usuario.getNombre());
                         }
-                        );
-        nombres.subscribe(e -> log.info(e), error -> log.error(error.getMessage()));
+                        )
+                .map(usuario -> {
+                    usuario.setNombre(usuario.getNombre().toUpperCase());
+                    return usuario;
+                })
+               .filter(usuario -> !usuario.getApellido().toUpperCase().equals("CONTRERA"));
+
+        nombres.subscribe(e -> log.info(e.toString()), error -> log.error(error.getMessage()));
     }
 }
---
+++ spring-boot-reactor/src/main/java/com/lab/reactor/model/Usuario.java
@@ -0,0 +1,15 @@
+package com.lab.reactor.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Usuario
+{
+    private String nombre;
+    private String apellido;
+
+}
